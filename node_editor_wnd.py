3861611cc9c0726dd704fdeb0f04b565d6388cf0
take000 <40623481+take000@users.noreply.github.com>
2020-03-17 02:15:21 +0900

    add DebugContet

--- node_editor_wnd.py
+++ node_editor_wnd.py
@@ -1,5 +1,9 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
 from node_graphics_scene import QDMGraphicsScene
+from node_graphics_view import QDMGraphicsView
 
 
 class NodeEditorWnd(QWidget):
@@ -16,12 +20,40 @@ class NodeEditorWnd(QWidget):
         self.setLayout(self.layout)
 
         # graphics scene
-        self.qtScene = QDMGraphicsScene()
+        self.grScene = QDMGraphicsScene()
 
         # graphics view
-        self.view = QGraphicsView(self)
-        self.view.setScene(self.qtScene)
+        self.view = QDMGraphicsView(self.grScene,self)
         self.layout.addWidget(self.view)
 
         self.setWindowTitle("Node Editor")
         self.show()
+
+        self.addDebugContent()
+
+    def addDebugContent(self):
+        redBrush = QBrush(Qt.red)
+        outlinePen = QPen(Qt.black)
+        outlinePen.setWidth(2)
+
+        rect = self.grScene.addRect(-100, -100, 80, 100, outlinePen, redBrush)
+        rect.setFlag(QGraphicsItem.ItemIsMovable)
+
+        text = self.grScene.addText("日本語でもOK!", QFont("Ubuntu"))
+        text.setFlag(QGraphicsItem.ItemIsSelectable)
+        text.setFlag(QGraphicsItem.ItemIsMovable)
+        text.setDefaultTextColor(QColor.fromRgbF(1.0, 1.0, 1.0))
+
+        widget1 = QPushButton("Hello World")
+        proxy1 = self.grScene.addWidget(widget1)
+        proxy1.setFlag(QGraphicsItem.ItemIsMovable)
+        proxy1.setPos(0, 30)
+
+        widget2 = QTextEdit()
+        proxy2 = self.grScene.addWidget(widget2)
+        proxy2.setFlag(QGraphicsItem.ItemIsSelectable)
+        proxy2.setPos(0, 60)
+
+        line = self.grScene.addLine(-200, -200, 400, -100, outlinePen)
+        line.setFlag(QGraphicsItem.ItemIsMovable)
+        line.setFlag(QGraphicsItem.ItemIsSelectable)
