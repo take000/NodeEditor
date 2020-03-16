928b382e1b3f1d8f46388439646831002afb3bcc
take000 <40623481+take000@users.noreply.github.com>
2020-03-16 22:05:40 +0900

    マスの追加

--- node_editor_wnd.py
+++ node_editor_wnd.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import *
+from node_graphics_scene import QDMGraphicsScene
 
 
 class NodeEditorWnd(QWidget):
@@ -8,4 +9,19 @@ class NodeEditorWnd(QWidget):
         self.initUI()
 
     def initUI(self):
+        self.setGeometry(560, 240, 800, 600)
+
+        self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(self.layout)
+
+        # graphics scene
+        self.qtScene = QDMGraphicsScene()
+
+        # graphics view
+        self.view = QGraphicsView(self)
+        self.view.setScene(self.qtScene)
+        self.layout.addWidget(self.view)
+
+        self.setWindowTitle("Node Editor")
         self.show()
